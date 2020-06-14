cb7e1918cda422ba83cab5c6cd1f2d6a232babd0
jih <Josh Herrera@LAPTOP-26A287TH.mit.edu>
2020-06-14 17:12:22 -0500

    Landing Page basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { 
-    path: 'gumby', loadChildren: () => import('./gumby/gumby.module').then(m => m.GumbyModule) 
-  },
-  {
-    path: '',
-    redirectTo: '',
-    pathMatch: 'full'
-  }
+  { path: 'gumby', loadChildren: () => import('./gumby/gumby.module').then(m => m.GumbyModule) },
+  { path: 'landing-page', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) },
+  { path: '', redirectTo: 'landing-page', pathMatch: 'full' }
+  
 ];
 
 @NgModule({
