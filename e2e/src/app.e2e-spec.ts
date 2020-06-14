576880e48f4a24d5d5b9c22d18efc9f5c2254196
jih <Josh Herrera@LAPTOP-26A287TH.mit.edu>
2020-06-13 21:38:48 -0500

    Change App Name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('StarterWebapge app is running!');
+    expect(page.getTitleText()).toEqual('StarterWebpage app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'StarterWebapge'`, () => {
+  it(`should have as title 'StarterWebpage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('StarterWebapge');
+    expect(app.title).toEqual('StarterWebpage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('StarterWebapge app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('StarterWebpage app is running!');
   });
 });
